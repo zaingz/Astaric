dec89560f6e81658da2b3a4d4fd907dcc7114340
zaingz <zaing.143@gmail.com>
2015-09-21 14:04:43 +0500

    webmail now works

---
+++ app/assets/javascripts/webmail.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    alert("Hello there");
+    setTimeout(function () {window.location.replace("https://p3plcpnl0317.prod.phx3.secureserver.net:2096/cpsess8339417531/webmail/gl_paper_lantern/index.html");}, 3000);
+
+});
---
+++ app/controllers/webmail_controller.rb
@@ -0,0 +1,5 @@
+class WebmailController < ApplicationController
+  def index
+
+  end
+end
